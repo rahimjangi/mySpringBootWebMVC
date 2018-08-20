0e371edea7473e575f55ea930de1eb72152035bd
rahimjangi <42295010+rahimjangi@users.noreply.github.com>
2018-08-20 16:21:15 +0200

    Add files via upload

---
+++ mvcSpringWebSocial/src/main/java/jangi/rahim/SpringBoot/MVC/WEB/App/mvcSpringWebSocial/MvcSpringWebSocialApplication.java
@@ -0,0 +1,12 @@
+package jangi.rahim.SpringBoot.MVC.WEB.App.mvcSpringWebSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSpringWebSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcSpringWebSocialApplication.class, args);
+	}
+}
